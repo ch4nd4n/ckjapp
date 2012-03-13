eef81959011526fefd91aebc49763f36549981ac
Chandan Kumar <chandankumar.r@gmail.com>
2012-03-13 15:10:41 +0530

    Added code to check for threads

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,12 +3,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
-
+import java.util.Date;
 public class HelloWorld extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        System.out.println(req);
+        System.out.println(new Date());
+        try{
+          Thread.currentThread().sleep(5000);
+        }catch(Exception e)
+        {System.out.println("I still failed");}
+        System.out.println(req + " over");
+        System.out.println(new Date());
         resp.getWriter().print("Hello from Java!\n");
     }
 
